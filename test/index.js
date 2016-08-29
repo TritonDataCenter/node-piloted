a0dd41ae2ff37cc44a7c723cee423ab97211b3de
Wyatt Preul <wpreul@gmail.com>
2016-08-29 15:45:57 -0500

    Adding tests

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@
 
 // Load modules
 
+const Http = require('http');
 const Code = require('code');
 const Lab = require('lab');
 const Piloted = require('..');
@@ -20,22 +21,89 @@ it('can be required', (done) => {
 });
 
 describe('config()', () => {
-  it('loads the configuration into piloted', (done) => {
+  it('loads the configuration into piloted and piloted can retrieve the cached information', (done) => {
+    const server = Http.createServer((req, res) => {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify([
+        { Service: { Address: 'nginx1.com', Port: '1234' } },
+        { Service: { Address: 'nginx2.com', Port: '1234' } }
+      ]));
+    });
+
+    server.listen(0, () => {
+      const config = {
+        consul: `localhost:${server.address().port}`,
+        backends: [
+          {
+            name: 'nginx'
+          },
+          {
+            name: 'app'
+          }
+        ]
+      };
+
+      Piloted.config(config, () => {
+        expect(Piloted('nginx').port).to.equal('1234');
+        done();
+      });
+    });
+  });
+
+  it('throws if the configuration is undefined', (done) => {
+    try {
+      Piloted.config();
+    } catch (ex) {
+      expect(ex).to.exist();
+      done();
+    }
+  });
+
+  it('throws if the configuration is missing backends', (done) => {
     const config = {
-      consul: 'consul:8500',
-      backends: [
-        {
-          name: 'nginx'
-        },
-        {
-          name: 'app'
-        }
-      ]
+      consul: 'consul:8500'
     };
 
-    Piloted.config(config, () => {
-      expect(Piloted('nginx')).to.exist();
+    try {
+      Piloted.config(config);
+    } catch (ex) {
+      expect(ex).to.exist();
       done();
+    }
+  });
+
+  it('returns an error on the callback if consul returns one', (done) => {
+    const server = Http.createServer((req, res) => {
+      res.writeHead(500);
+      res.end();
     });
+
+    server.listen(0, () => {
+      const config = {
+        consul: `localhost:${server.address().port}`,
+        backends: [
+          {
+            name: 'backend'
+          }
+        ]
+      };
+
+      Piloted.config(config, (err) => {
+        expect(err).to.exist();
+        done();
+      });
+    });
+  });
+});
+
+
+describe('Piloted()', () => {
+  it('throws if the backend service isn\'t configured', (done) => {
+    try {
+      Piloted('notknown');
+    } catch (ex) {
+      expect(ex).to.exist();
+      done();
+    }
   });
 });
